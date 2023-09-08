f83fde13e6e885511faaee727fd99f6d0a157d19
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2023-09-07 20:31:46 -0400

    Moved old code base to aux folder

--- src/prairie/slack/__init__.py
+++
@@ -1,7 +0,0 @@
-
-"""
-This submodule contains all the code that interacts directly with the
-`Slack API <https://github.com/seratch/python-slack-scim>`_  and
-`Slack SCIM API <https://github.com/seratch/python-slack-scim>`_ official
-Python SDKs.
-"""
